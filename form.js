1992917f023d96499966e37b7b37bb872b2af280
Nick Colstad <93539613+nickcolstad@users.noreply.github.com>
2022-03-21 21:40:03 -0400

    Add files via upload

---
+++ form.js
@@ -0,0 +1,16 @@
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} placeholder="Add item" onChange={e => setValue(e.target.value)} />
+    </form>
+  );
+}
